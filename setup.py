75a12c73a6fcceff49db3b023c0f86b56d505575
r0manch1k <romansokolv209@gmail.com>
2025-04-27 23:37:57 +1000

    prepare for release

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cmlabs",
+    version="0.1.0",
+    author="Roman Sokolovsky",
+    author_email="romansokolv209@gmail.com",
+    description="A library for computational mathematics labs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/r0manch1k/cmlabs",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.21.0",
+        "scipy>=1.7.0",
+    ],
+)
